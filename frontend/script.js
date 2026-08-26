9019f17098bf54e89d39a19c519791824a5af3f8
JAY LORENCE BELVIATURA PAELDIN <157566097+Lawrenzjay@users.noreply.github.com>
2026-08-26 23:50:06 +0800

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -30,7 +30,7 @@ const statusText =
 // ==========================================
 
 const FLASK_BASE_URL =
-    "https://jarvis-ai-black-nine.vercel.app";
+    "https://jarvis-ai-g3.vercel.app";
 
 const CHAT_URL = `${FLASK_BASE_URL}/chat`;
 const CLEAR_URL = `${FLASK_BASE_URL}/clear`;
